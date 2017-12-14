b71548f2d9a460765f3dc8676d94b97231957c41
Matthew Shampine <mshampine4@gmail.com>
2017-12-14 16:31:47 -0500

    Uploaded code for access

---
+++ Sources/main.swift
@@ -0,0 +1,102 @@
+import Foundation
+
+class Calculator {
+    var total: Double
+    var operation: String
+    var operand: Double?
+    var validOps = "+-*/^"
+
+    init() {
+        total = 0.0
+        operation = ""
+        operand = 0.0
+    }
+
+    func run() {
+        while(true) {
+            print("Enter an operator: " + validOps)
+            if let input = readLine() {
+                let trimInput = input.trimmingCharacters(in: .whitespacesAndNewlines)
+
+                if trimInput.count == 1 {
+                    let op = trimInput[trimInput.startIndex]
+                
+                    if checkOperation(input: op) {
+                        operation = String(op)
+                        break
+                    }
+                }
+            } else {
+                print("Hit Ctl+C to exit.")
+            }
+        }
+
+        while(true) {
+            print("Enter an operand:")
+            if let input = readLine() {
+                let operand = NumberFormatter().number(from: input)?.doubleValue
+
+                if operand != nil {
+                    let result =  calculate(input: operand!)
+                    if result.0 {
+                       print(result.1)
+                       break
+                    } else {
+                       print(result.2)
+                    }
+                }
+            } else {
+                print("Hit Ctl+C to exit.")
+            }
+        }
+    }
+
+    func checkOperation(input: Character) -> Bool {
+        for char in validOps {
+            if (input == char) {
+                return true
+            }
+        }
+        return false
+    }
+
+    func calculate(input: Double) -> (Bool, Double, String) {
+        switch operation {
+        case "+":
+            total = self.total + input
+            break
+        case "-":
+            total = self.total - input
+            break
+        case "*":
+            total = self.total * input
+            break
+        case "/":
+            let temp  = total / input
+            if temp.isNaN || temp.isInfinite {
+                return (false, 0.0, "Error cannot divide by zero")
+            } else {
+                total = temp
+                break
+            }
+        case "^":
+            if input < 0 {
+                return (false, 0.0, "Error cannot use negative exponents")
+            } else {
+                total = pow(self.total, input)
+                break
+            }
+        default:
+            break
+        }
+        return (true, total, "success")
+    }
+}
+
+print("Welcome to Matt's Basic Calculator in Swift")
+print("Hit Ctrl+C to exit.")
+let main = Calculator()
+while (true) {
+    main.run()
+}
+
